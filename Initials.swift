a143b72c2290ea8db202c646986aaee6dc2f5724
mikedpayne <mikedpayne@gmail.com>
2020-05-14 09:42:52 -0400

    Create Initials.swift

---
+++ Initials.swift
@@ -0,0 +1,19 @@
+// I am just learning Swift
+
+print("M   M")
+print("MM MM")
+print("MM MM")
+print("M M M")
+print("M   M")
+print("M   M")
+print("M   M")
+print("")
+print("PPPP ")
+print("P    P")
+print("P    P")
+print("PPPP ")
+print("P")
+print("P")
+print("P")
+print("")
+

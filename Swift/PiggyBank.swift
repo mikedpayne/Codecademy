dabea0bdb459b6f79d5137b6f654253003a01d14
mikedpayne <mikedpayne@gmail.com>
2020-05-21 12:01:05 -0400

    Create PiggyBank.swift

---
+++ Swift/PiggyBank.swift
@@ -0,0 +1,13 @@
+var pesos : Double = 4
+var reais : Double = 5
+var soles : Double = 6
+
+// 0.044 pesos to a dollar
+// 0.18 reais to a dollar
+// 0.29 soles to a dollar
+
+var total : Double
+
+total = (pesos * 0.044) + (reais * 0.18) + (soles * 0.29)
+
+print("Total USD: $\(total)")

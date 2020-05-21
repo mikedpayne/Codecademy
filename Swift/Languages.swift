789c04558392a0440a70ba088b25993a2df68f8a
mikedpayne <mikedpayne@gmail.com>
2020-05-21 13:13:56 -0400

    Create Languages.swift

---
+++ Swift/Languages.swift
@@ -0,0 +1,21 @@
+// Write your code below 🗽
+/*
+English: EN
+Spanish: ES
+Chinese: ZH
+Russion: RU
+*/
+
+var abbreviation = "EN"
+
+if abbreviation == "EN" {
+  print("English")
+} else if abbreviation == "ES" {
+  print("Spanish")
+} else if abbreviation == "ZH" {
+  print("Chinese")
+} else if abbreviation == "RU" {
+  print("Russian")
+} else {
+  print("Abbreviation not found")
+}

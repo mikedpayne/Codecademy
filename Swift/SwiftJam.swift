874f3b4a480c1e99af0351e58f2c0291d3ec9775
mikedpayne <mikedpayne@gmail.com>
2020-05-21 13:08:40 -0400

    Create SwiftJam.swift

---
+++ Swift/SwiftJam.swift
@@ -0,0 +1,10 @@
+let tuneSquadPoints = 78
+let monstarsPoints = 77
+
+// Write your code below 🏀
+
+if tuneSquadPoints < monstarsPoints {
+  print("Congrats monstars!")
+} else {
+  print("Congrats tuneSquad!")
+}

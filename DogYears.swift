659da7504e944c8663ede1e79171ff3f514174f0
mikedpayne <mikedpayne@gmail.com>
2020-05-21 11:47:45 -0400

    Create DogYears.swift

---
+++ DogYears.swift
@@ -0,0 +1,14 @@
+var dogAge = 10 // age of my dog
+var earlyYears = 2
+var laterYears = 8
+var humanYears : Int
+
+earlyYears = 21 // first two years of a dog's age are 21 human years
+
+laterYears = (dogAge - 2) * 4 // rest of a dog's age
+
+humanYears = earlyYears + laterYears; // total
+
+print (humanYears)
+
+print("my name is ____! Ruff Ruff, I am \(humanYears) years old in human years.")

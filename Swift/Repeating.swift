9b6f3bdffb4f121e4f665bcbaed247ce84a3acbb
mikedpayne <mikedpayne@gmail.com>
2020-05-22 15:25:18 -0400

    Create Repeating.swift

---
+++ Swift/Repeating.swift
@@ -0,0 +1,19 @@
+// Add your code below 😈
+
+
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")

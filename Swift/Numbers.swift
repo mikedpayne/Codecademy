a2a3a65277967a44cd3d8b281e0431ca23dbfb27
mikedpayne <mikedpayne@gmail.com>
2020-05-21 22:06:49 -0400

    Create Numbers.swift

---
+++ Swift/Numbers.swift
@@ -0,0 +1,11 @@
+var wholeNumber = Int.random(in: 10...20)
+
+// Write your code below 
+switch wholeNumber {
+  case let x where x % 2 == 0:
+    print("Composite")
+  case let x where x % 3 == 0:
+    print("Composite")
+  default:
+    print("Prime")
+}

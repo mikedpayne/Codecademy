fe0e041e45549e3ceea172f693e6580dc8045fdb
mikedpayne <mikedpayne@gmail.com>
2020-05-22 22:46:36 -0400

    Create NewYearNewMe.swift

---
+++ Swift/NewYearNewMe.swift
@@ -0,0 +1,9 @@
+var resolutions = ["play more music 🎸",
+                   "read more books 📚",
+                   "drink more water 💧"]
+
+// Write your code below 💪
+
+resolutions.append("Learn Swift")
+resolutions += ["Develop an iPhone app"]
+print(resolutions)

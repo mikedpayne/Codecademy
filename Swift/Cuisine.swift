862e26d65e3689c5dba89effe13d9e89130adbf7
mikedpayne <mikedpayne@gmail.com>
2020-06-04 15:11:40 -0400

    Create Cuisine.swift

---
+++ Swift/Cuisine.swift
@@ -0,0 +1,8 @@
+// Write your code below 😋
+func favoriteCuisine() -> (name: String, dish: String) {
+  return ("Italian", "Pizza")
+}
+
+let cuisine = favoriteCuisine()
+
+print("My favorite \(cuisine.name) dish is \(cuisine.dish)")

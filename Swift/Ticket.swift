2575e51ea0d982c1ce48c26773ddd1b602885e9d
mikedpayne <mikedpayne@gmail.com>
2020-06-04 20:05:27 -0400

    Create Ticket.swift

---
+++ Swift/Ticket.swift
@@ -0,0 +1,8 @@
+// Write your code below 🎫
+
+func bookingTicket(passengerName: String = "Mike", seatClass: String = "Economy", timeOfDeparture: Int) -> String {
+  return "\(passengerName), your seat class is \(seatClass), and you will be departing at \(timeOfDeparture)."
+}
+
+print(bookingTicket(timeOfDeparture: 9))
+print(bookingTicket(seatClass: "Business", timeOfDeparture: 9))

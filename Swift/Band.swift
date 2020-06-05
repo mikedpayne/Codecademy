1ed3a8c7e0c421969cf1a6cb167a42dc7ebe21d4
mikedpayne <mikedpayne@gmail.com>
2020-06-05 16:34:00 -0400

    Update Band.swift

--- Swift/Band.swift
+++ Swift/Band.swift
@@ -1,8 +1,31 @@
-// Add your code below 🎹 🎸 🥁 
 struct Band {
   var genre: String
   var members: Int
   var isActive: Bool
+  
+  init(genre: String, members: Int, isActive: Bool) {
+    self.genre = genre
+    self.members = members
+    self.isActive = isActive
+  }
+  
+  func pumpUpCrowd() -> String {
+    if self.isActive {
+      return "Are you ready to ROCK?"
+    } else {
+      return "..."
+    }
+  }
+  
+  mutating func changeGenre(newGenre: String) -> String {
+    self.genre = newGenre
+    return self.genre
+  } 
+  
 }
 
-var maroon5 = Band(genre: "pop", members: 5, isActive: true)
+var journey = Band(genre: "rock", members: 5, isActive: true)
+
+// Add your code below 📻  
+print(type(of: journey))
+

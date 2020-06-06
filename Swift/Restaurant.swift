595e1954ac4c98f1ea34685710b6542f13e322e2
mikedpayne <mikedpayne@gmail.com>
2020-06-06 16:59:35 -0400

    Update Restaurant.swift

--- Swift/Restaurant.swift
+++ Swift/Restaurant.swift
@@ -1,7 +1,18 @@
-// Write your code below 🍝
 class Restaurant {
   var name = ""
   var type = [""]
   var rating = 0.0
   var delivery = false
 }
+
+// Write your code below 🍝
+var bobsBurgers = Restaurant()
+bobsBurgers.name = "Bob's Burgers"
+bobsBurgers.type = ["Burgers", "Fast Food"]
+bobsBurgers.rating = 3.5
+bobsBurgers.delivery = true
+
+print(bobsBurgers.name)
+print(bobsBurgers.type)
+print(bobsBurgers.rating)
+print(bobsBurgers.delivery)

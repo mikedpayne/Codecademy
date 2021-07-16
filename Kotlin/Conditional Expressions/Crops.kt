fe312c77a2921fa016ee3011d7e97cfe83a46dcd
mikedpayne <mikedpayne@gmail.com>
2021-07-16 18:19:23 -0400

    Create Crops.kt

---
+++ Kotlin/Conditional Expressions/Crops.kt
@@ -0,0 +1,13 @@
+fun main() {
+  var season = "Spring"
+  // Write your code below
+ when (season) {
+   "Fall" -> println("Grow pumpkins.")
+   "Summer" -> println("Grow corn.")
+   "Spring" -> println("Grow lettuce.")
+   "Winter" -> println("Grow kale.")
+   else -> println("Not a valid season.")
+ }
+
+ 
+}

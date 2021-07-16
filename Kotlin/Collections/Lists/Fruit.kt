7de7d33c43799122164ae157fce56321fb8ba6b3
mikedpayne <mikedpayne@gmail.com>
2021-07-16 18:24:51 -0400

    Create Fruit.kt

---
+++ Kotlin/Collections/Lists/Fruit.kt
@@ -0,0 +1,6 @@
+fun main() {
+  // Write your code below 🍎
+  val fruitTrees = mutableListOf("Apple","Plum","Pear","Cherry")
+  
+  print("I am growing ${fruitTrees.size} different types of fruit in my garden.")
+}

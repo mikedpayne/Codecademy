4cd3552db5ed4ab6bd61d5083af1074a79cc030a
mikedpayne <mikedpayne@gmail.com>
2021-07-16 17:34:57 -0400

    Create Date.kt

---
+++ Kotlin/Data Types and Variables/Date.kt
@@ -0,0 +1,9 @@
+fun main() {
+  // Write your code below ☀️
+  var todaysDate: String = "03/05/2021"
+  println(todaysDate)
+
+  var currentWeather: String
+  currentWeather = "Sunny"
+  println(currentWeather)
+}

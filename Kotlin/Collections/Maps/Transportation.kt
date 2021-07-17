976551bd00ec22b9c12f8444a9ea117a3d1216cb
mikedpayne <mikedpayne@gmail.com>
2021-07-17 10:49:32 -0400

    Update Transportation.kt

--- Kotlin/Collections/Maps/Transportation.kt
+++ Kotlin/Collections/Maps/Transportation.kt
@@ -2,6 +2,8 @@ fun main() {
   var uniqueTransport = mapOf("India" to "Tuktuk", "Egypt" to "Felucca", "Peru" to "Totora Boat", "Hong Kong" to "Junk Boat")
 
   // Write your code below 🚜
-  print("A unique mode of transportation that exists in Peru is ${uniqueTransport["Peru"]}.")
+  println("A unique mode of transportation that exists in Peru is ${uniqueTransport["Peru"]}.")
 
+  println(uniqueTransport.keys)
+  println(uniqueTransport.values)
 }

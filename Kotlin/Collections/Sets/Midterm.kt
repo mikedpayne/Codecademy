6cc91982ab68e160f01c8c70797f2888067bbf23
mikedpayne <mikedpayne@gmail.com>
2021-07-17 10:39:41 -0400

    Create Midterm.kt

---
+++ Kotlin/Collections/Sets/Midterm.kt
@@ -0,0 +1,15 @@
+fun main() {
+  // Write your code below 📝
+  var testGrades = mutableSetOf(65,50,72,80,53,84)
+
+  var sum = testGrades.sum()
+  var numStudents = testGrades.size
+  var average = sum / numStudents
+
+  if (average < 65) {
+    print("Failed")
+  } else {
+    print("Passed")
+  }
+
+}

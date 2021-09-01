ce5b551792a3eb185b614731f03d5c58fda39a12
mikedpayne <mikedpayne@gmail.com>
2021-08-31 21:30:13 -0400

    Update Recipe.kt

--- Kotlin/Functions/Recipe.kt
+++ Kotlin/Functions/Recipe.kt
@@ -4,6 +4,11 @@
 
 fun main() {
   // Write more code below
+  var tspNeeded = ozToTsp(.75)
+  println("You will need $tspNeeded tsp of vanilla extract for this recipe.")
+}
 
-  
+fun ozToTsp(oz: Double): Double {
+  var tsp = oz * 6
+  return tsp
 }

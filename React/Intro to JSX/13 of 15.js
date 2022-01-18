911fbf3524072d2f5d6d2a8b648b1a67f59b96be
Mike Payne <mikedpayne@gmail.com>
2022-01-18 15:37:45 -0500

    added files

---
+++ React/Intro to JSX/13 of 15.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (
+  <ul>
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </ul>
+)
+
+ReactDOM.render(myList, document.getElementById('app'));
